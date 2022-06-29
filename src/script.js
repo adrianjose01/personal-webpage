4bdcffd76b3e65272afa8a1718e4fec5a5f09033
Akiyama Joseph <joseph.estevez@protonmail.com>
2022-06-29 15:16:42 -0400

    format

--- src/script.js
+++ src/script.js
@@ -1,18 +1,18 @@
-const elements = document.querySelectorAll(".element");
+const elements = document.querySelectorAll('.element')
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      entry.target.classList.toggle("show", entry.isIntersecting);
-      if (entry.isIntersecting) observer.unobserve(entry.target);
-    });
+      entry.target.classList.toggle('show', entry.isIntersecting)
+      if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
   },
   {
     threshold: 0.5,
-    rootMargin: "100px",
+    rootMargin: '100px'
   }
-);
+)
 
 elements.forEach((el) => {
-  observer.observe(el);
-});
+  observer.observe(el)
+})
